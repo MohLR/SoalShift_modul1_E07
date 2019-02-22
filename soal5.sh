e833a18b2e95eecd83bd455e2c904bdb910cbb53
Arino Jenynof <arino.j17@gmail.com>
2019-02-22 17:23:08 +0700

    Salah upload file :v

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cat /var/log/syslog | awk '/cron/ && !/sudo/' | awk 'NF < 13' >> /home/arino/modul1
+mkdir /home/$USER/modul1
+cat /var/log/syslog | awk '/cron/ && !/sudo/' | awk 'NF < 13' >> /home/$USER/modul1/$(date +%Y%m%d%H%M%S).log
